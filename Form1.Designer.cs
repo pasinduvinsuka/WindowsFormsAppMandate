fea9a25f140e2d6ce9e78ffe4d1d87b53821862a
Pasindu Vinsuka <88902687+pasinduvinsuka@users.noreply.github.com>
2023-04-21 09:56:59 +0530

    button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(412, 234);
+            this.button1.Location = new System.Drawing.Point(384, 231);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(222, 158);
+            this.button1.Size = new System.Drawing.Size(219, 56);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
